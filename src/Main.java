44d45f9f6da8fff0b07e260a94a884aeda0e4862
João Vitor <joaogepe@gmail.com>
2025-04-15 00:43:01 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import brComDioDesafio.dominio.Curso;
+import brComDioDesafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
 
+        Curso curso1= new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("JavaScript");
+        curso2.setDescricao("Descrição curso JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
 
     }
 }
